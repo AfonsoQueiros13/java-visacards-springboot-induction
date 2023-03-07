8e33329a87580831750745b6ee34b2cf27559033
Afonso Queirós <ext.afonso.queiros@farfetch.com>
2023-03-07 12:37:17 +0000

    [main] Initial Commit

---
+++ visacards-api/src/main/java/com/afonsoqueiros/springbootinduction/visacardsapi/VisacardsApiApplication.java
@@ -0,0 +1,13 @@
+package com.afonsoqueiros.springbootinduction.visacardsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisacardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisacardsApiApplication.class, args);
+	}
+
+}
---
+++ visacards-api/src/test/java/com/afonsoqueiros/springbootinduction/visacardsapi/VisacardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afonsoqueiros.springbootinduction.visacardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisacardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
