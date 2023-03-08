3a7131ab48634e961e197136a37d7648548c74c5
Carlos Ferreira <carlos.eds.ferreira@gmail.com>
2023-03-08 13:07:27 +0000

    My fix

--- visacards-api/src/main/java/com/afonsoqueiros/springbootinduction/visacardsapi/VisaCardsApiApplication.java
+++ visacards-api/src/main/java/com/afonsoqueiros/springbootinduction/visacardsapi/VisaCardsApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-
 public class VisaCardsApiApplication {
 
 	public static void main(String[] args) {
